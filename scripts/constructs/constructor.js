c4bc97d27068ede0de70379aae0a94e59623f05e
Patrick Liesveld <pliesveld@users.noreply.github.com>
2018-01-21 09:39:33 -0500

    Added constructor + call example
    When Product is invoked, javascript creates a new object, with a prototype of Product.prototype  and then calls the Product function.
    
    lets the Food constructor do all the same stuff that the Product constructor does, plus a bit of extra stuff. It's a way to reuse the code you wrote for Product, while adding specialized behavior that only matters to Food items.

--- scripts/constructs/constructor.js
+++ scripts/constructs/constructor.js
@@ -19,3 +19,19 @@ console.log(Object.getPrototypeOf(ford));
 // console.log(Object.getPrototypeOf(ford) === Car);
 console.log(Object.getPrototypeOf(ford) === Car.prototype);
 
+console.log('******************');
+
+function Product(name, price) {
+	this.name = name;
+	this.price = price;
+}
+
+function Food(name, price) {
+	Product.call(this, name, price);
+	this.category = 'food';
+}
+
+console.log(new Product('apples', 5));
+console.log(new Food('Pizza', 26));
+
+
